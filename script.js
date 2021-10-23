8eba058e624ccd4f88f0b5ad88e22fbeed61462e
LATOMMYLA <tommyl.dmd@gmail.com>
2021-10-22 19:57:01 -0700

    final changes and comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// variables
 var city = ""
 var citySearch = $("#citySearch")
 var searchBtn = $("#searchBtn")
@@ -18,6 +19,7 @@ function find(c) {
   return 1;
 }
 
+// apiKey variable with openweather api key
 var apiKey = "cbd797a924ad0cf3d6679e50d79ed644"
 
 function displayWeather(event) {
@@ -28,6 +30,7 @@ function displayWeather(event) {
   }
 }
 
+// function for currentWeather with api url to grab current weather data
 function currentWeather(city) {
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
   
@@ -37,12 +40,15 @@ function currentWeather(city) {
     })
     .then(function(response){
 
+    // variables for weather icon and date
     var weathericon = response.weather[0].icon;
+    // url to weather icons
     var iconurl = "http://openweathermap.org/img/w/" + weathericon + ".png";
     var date = new Date(response.dt*1000).toLocaleDateString();
 
     $(currentCity).html(response.name + "("+date+")" + "<img src=" +iconurl+ ">");
 
+    // current temperature, humidity and wind speed
     var tempF = (response.main.temp - 273.15) * 1.80 + 32;
     $(currentTemperature).html((tempF).toFixed(0) + "&#8457");
     $(currentHumidity).html(response.main.humidity + "%");
@@ -52,7 +58,7 @@ function currentWeather(city) {
 
     uvIndex(response.coord.lon, response.coord.lat);
     forecast(response.id);
-    if(response.cod === 200) {
+    if(response.cod == 200) {
       sCity = JSON.parse(localStorage.getItem("cityname"));
       console.log(sCity);
       if (sCity == null) {
@@ -69,12 +75,14 @@ function currentWeather(city) {
         }
       }
     }
-  })
+  });
 }
 
-function uvIndex(ln, lt) {
-  var uvURL = "https://api.openweathermap.org/data/2.5/onecall?appid=" + apiKey + "&lat=" + lt + "&lon=" + ln + "&exclude=current";
+// uvIndex function with api url from openweathermap
+function uvIndex() {
+  var uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclud=current.uvi&appid=16fe336e7e13affed902f5c51534a4d3";
 
+  // fetch url
   fetch(uvURL)
     .then(function(response) {
       return response.json();
@@ -84,9 +92,11 @@ function uvIndex(ln, lt) {
     });
 }
 
+// 5 day forecast function with api url from openweathermap
 function forecast(cityid) {
   var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&appid=" + apiKey;
 
+  // fetch url
   fetch(forecastURL)
     .then(function(response) {
       return response.json();
@@ -97,11 +107,11 @@ function forecast(cityid) {
     for (i = 0; i < 5; i++) {
       var date = new Date((response.list[((i+1) * 8) -1].dt) *1000).toLocaleDateString();
       var iconcode = response.list[((i+1) * 8) -1].weather[0].icon;
-      var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+      var iconurl = "https://openweathermap.org/img/wn/" + iconcode + "@2x.png";
       var tempK = response.list[((i+1) * 8) -1].main.temp;
       var tempF = (((tempK - 273.5) * 1.80) + 32).toFixed(0);
       var humidity = response.list[((i+1) * 8) -1].main.humidity;
-
+      // for each card-body within the 5 day forecast
       $("#date"+i).html(date);
       $("#image"+i).html("<img src=" + iconurl + ">");
       $("#temp"+i).html(tempF + "&#8457");
@@ -110,8 +120,10 @@ function forecast(cityid) {
   });
 }
 
+// function to add recent searches
 function addToList(c) {
   var listEl = $("<li>" + c.toUpperCase() + "</li>");
+  // list element created
   $(listEl).attr("class", "list-group-item");
   $(listEl).attr("data-value", c.toUpperCase());
   $(".list-group").append(listEl);
@@ -138,6 +150,7 @@ function lastCity(){
   }
 }
 
+// clear search history function when button is clicked
 function clearHistory(event) {
   event.preventDefault();
   sCity = [];
@@ -145,6 +158,7 @@ function clearHistory(event) {
   document.location.reload();  
 }
 
+// on click and load functions
 $("#searchBtn").on("click", displayWeather);
 $(document).on("click", pastSearch);
 $(window).on("load", lastCity);
